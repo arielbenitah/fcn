bd3b7989c731a29d228562ca0e2137ae6736bc7d
Ariel Benitah <ariel.benitah@gmail.com>
2017-04-18 13:57:04 +0300

    utils

---
+++ fcn_utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from keras.utils.layer_utils import layer_from_config
+from keras.preprocessing import image
+from keras.utils.np_utils import to_categorical
+
+
+def get_batches(dirname, gen=image.ImageDataGenerator(), shuffle=True, batch_size=4, class_mode='categorical',
+                target_size=(224,224)):
+    return gen.flow_from_directory(dirname, target_size=target_size,
+            class_mode=class_mode, shuffle=shuffle, batch_size=batch_size)
+
+def onehot(x):
+    return to_categorical(x)
+
+def get_data(path, target_size=(224,224)):
+    batches = get_batches(path, shuffle=False, batch_size=1, class_mode=None, target_size=target_size)
+    return np.concatenate([batches.next() for i in range(batches.nb_sample)])
+
+def plot(img):
+    plt.imshow(to_plot(img))
+
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, len(ims)//rows, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
+
+
